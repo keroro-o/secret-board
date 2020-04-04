f59db569a4028fa6fe368ce88e0c8b56d6d6bd39
keroro-o <obata19760205akinobu@outlook.jp>
2020-04-04 20:17:19 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('hi');
+}).on('error', (e) => {
+  console.error('Server Error', e);
+}).on('clientError', (e) => {
+  console.error('Client Error', e);
+});
+
+const port = 8000;
+server.listen(port, () => {
+  console.info(`ポート ${port} 番でサーバーを起動しました`);
+});
